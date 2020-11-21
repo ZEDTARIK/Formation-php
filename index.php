aa49018c3d9204e54ddb0e069b8f5cb9ecd3d1fd
ettarak <ettarak.zouhair@gmail.com>
2020-11-21 15:43:49 +0100

    Begging Formation php pure

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$title = "Formation php";
+define('NAME', 'Variable Constant');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+</head>
+
+<body>
+    <h2><?php echo $title; ?></h2>
+    <pre><?php echo NAME; ?></pre>
+</body>
+
+</html>
